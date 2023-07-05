0599a1c337188b9b2cd71acf3fac3a3575231d30
Shubham Gupta <104024620+shubh377@users.noreply.github.com>
2023-07-06 02:15:02 +0530

    Time: 1329 ms (5.01%), Space: 67.8 MB (38.99%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -0,0 +1,66 @@
+class MedianFinder {
+    
+//     List<Integer> list;
+    
+//     public MedianFinder() {
+//         this.list=new ArrayList<>();
+//     }
+    
+//     public void addNum(int num) {
+        
+//         if (list.size()==0){
+//             list.add(num);
+//         }
+//         else{
+//             int i=0;
+            
+//             while (i<=list.size()-1 && list.get(i)<=num ){
+//                 i++;
+//             }
+            
+//             list.add(i,num);
+//         }
+//     }
+    
+//     public double findMedian() {
+        
+//         int n=list.size();
+//         if (n%2==0){
+//             int i=n/2;
+//             double sum=(double)list.get(i)+(double)list.get(i-1);
+//             return sum/2;
+//         }
+//         if (n==1){
+//             return (double)list.get(0);
+//         }
+//         return (double)list.get((n/2)-1);
+//     }
+    List<Integer> list; 
+    public MedianFinder() {
+        this.list = new ArrayList<Integer>();
+    }
+    
+    public void addNum(int num) {
+        if(list.size() == 0) list.add(0, num);  
+        else{
+            int i=0;
+            while(list.get(i) <= num && i<list.size()-1) i++;
+            if(list.get(i) <= num) list.add(i+1, num);
+            else list.add(i, num);
+        }
+    }
+    
+    public double findMedian() {
+        int n = list.size();
+        if(n == 0) return 0;
+        if(n%2 == 1) return list.get(n/2);
+        return (double)((list.get(n/2) + list.get(n/2 - 1)))/2;
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
