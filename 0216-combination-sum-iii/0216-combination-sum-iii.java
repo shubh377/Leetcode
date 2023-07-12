bbca9d747ba442dbf8151cfa4e161abedf1455bf
Shubham Gupta <104024620+shubh377@users.noreply.github.com>
2023-07-13 02:23:25 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (84.03%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,25 +1,23 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         
-         List<List<Integer>> ans = new ArrayList<>();
-        ArrayList<Integer> curr = new ArrayList<>();
-        generatecombination(1,9,curr,ans,k,n);
-
-        return ans;
+        List<List<Integer>> list=new ArrayList<>();
+        List<Integer> curr=new ArrayList<>();
+        
+        gencom(list,curr,k,n,1,9);
+        
+        return list;
     }
-
-    public void generatecombination(int start,int end,ArrayList<Integer> curr, List<List<Integer>> ans,int k, int target){
-
-        if(target == 0 && curr.size() == k){
-            ans.add(new ArrayList(curr));
+    
+    public static void gencom(List<List<Integer>> list,List<Integer> curr,int k,int n,int start,int end){
+        
+        if (n==0 && curr.size()==k){
+            list.add(new ArrayList<>(curr));
         }
-
         
         for(int i = start; i <= end; i++){
             curr.add(i);
-
-            generatecombination(i+1, end, curr,ans,k,target-i);
-
+            gencom(list,curr,k,n-i,i+1,end);
             curr.remove(curr.size() - 1);
         }
     }
